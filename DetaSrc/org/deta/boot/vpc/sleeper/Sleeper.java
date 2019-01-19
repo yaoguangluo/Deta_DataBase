222c7eb2b575b7440c86b959821d8699ea112ff7
yaoguangluo <fengyue1985>
2019-01-19 22:58:28 +0800

    invalid setRoot notice

--- DetaSrc/org/deta/boot/vpc/sleeper/Sleeper.java
+++ DetaSrc/org/deta/boot/vpc/sleeper/Sleeper.java
@@ -33,8 +33,8 @@ public class Sleeper extends Thread implements Runnable{
 			
 		}catch(Exception e){
 			try {
-				e.printStackTrace();
 				vPCSResponse.returnErrorCode(500);
+				e.printStackTrace();
 			} catch (IOException e1) {
 				System.gc();
 				e1.printStackTrace();
