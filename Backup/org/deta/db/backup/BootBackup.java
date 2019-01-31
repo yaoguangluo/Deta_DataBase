42f83e2f28b8179b958116c8d5d06e3c6b6e1edd
yaoguangluo <fengyue1985>
2019-01-31 08:24:50 +0800

    untest and uncheck

--- Backup/org/deta/db/backup/BootBackup.java
+++ Backup/org/deta/db/backup/BootBackup.java
@@ -1,7 +1,93 @@
 package org.deta.db.backup;
 
-public class BootBackup {
-	public static void bootAllWithGzipByTime(){
-		
-	}
+import java.io.BufferedInputStream;  
+import java.io.BufferedOutputStream;  
+import java.io.File;  
+import java.io.FileInputStream;  
+import java.io.FileNotFoundException;  
+import java.io.FileOutputStream;  
+import java.io.IOException;  
+import java.util.zip.ZipEntry;  
+import java.util.zip.ZipOutputStream;
+
+import org.lyg.cache.CacheManager;  
+
+/** 
+ *  uncheck and untest
+ * @author https://wenda.so.com/q/1512966734215123 gets
+ * @author yaoguangluo fix
+ * @author aczjc refer
+ * 
+ */  
+
+public final class BootBackup {  
+	private BootBackup(){}   
+	public static void BootBackupByUsingGzip(String zipPath){  
+		String sourceFilePath = CacheManager.getCacheInfo("DBPath").getValue().toString();
+		String zipFilePath = zipPath;  
+		long time = System.currentTimeMillis();
+		String fileName = "zip_" + time;  
+		boolean flag = BootBackup.fileToZip(sourceFilePath, zipFilePath, fileName);  
+		if(flag){  
+			System.out.println("文件打包成功!");  
+		}else{  
+			System.out.println("文件打包失败!");  
+		}  
+	}     
+
+	public static boolean fileToZip(String sourceFilePath, String zipFilePath, String fileName){  
+		boolean flag = false;  
+		File sourceFile = new File(sourceFilePath);  
+		FileInputStream fileInputStream = null;  
+		BufferedInputStream bufferedInputStream = null;  
+		FileOutputStream fileOutputStream = null;  
+		ZipOutputStream zipOutputStream = null;     
+		if(sourceFile.exists() == false){  
+			System.out.println("待压缩的文件目录：" + sourceFilePath + "不存在.");  
+		}else{  
+			try {  
+				File zipFile = new File(zipFilePath + "/" + fileName + ".zip");  
+				if(zipFile.exists()){  
+					System.out.println(zipFilePath + "目录下存在名字为:" + fileName +".zip" +"打包文件.");  
+				}else{  
+					File[] sourceFiles = sourceFile.listFiles();  
+					if(null == sourceFiles || sourceFiles.length<1){  
+						System.out.println("待压缩的文件目录：" + sourceFilePath + "里面不存在文件，无需压缩.");  
+					}else{  
+						fileOutputStream = new FileOutputStream(zipFile);  
+						zipOutputStream = new ZipOutputStream(new BufferedOutputStream(fileOutputStream));  
+						byte[] bufs = new byte[1024*10];  
+						for(int i=0;i<sourceFiles.length;i++){  
+							//创建ZIP实体，并添加进压缩包  
+							ZipEntry zipEntry = new ZipEntry(sourceFiles[i].getName());  
+							zipOutputStream.putNextEntry(zipEntry);  
+							//读取待压缩的文件并写进压缩包里  
+							fileInputStream = new FileInputStream(sourceFiles[i]);  
+							bufferedInputStream = new BufferedInputStream(fileInputStream, 1024*10);  
+							int read = 0;  
+							while((read = bufferedInputStream.read(bufs, 0, 1024*10)) != -1){  
+								zipOutputStream.write(bufs,0,read);  
+							}  
+						}  
+						flag = true;  
+					}  
+				}  
+			} catch (FileNotFoundException e) {  
+				e.printStackTrace();  
+				throw new RuntimeException(e);  
+			} catch (IOException e) {  
+				e.printStackTrace();  
+				throw new RuntimeException(e);  
+			} finally{  
+				try {  
+					if(null != bufferedInputStream) bufferedInputStream.close();  
+					if(null != zipOutputStream) zipOutputStream.close();  
+				} catch (IOException e) {  
+					e.printStackTrace();  
+					throw new RuntimeException(e);  
+				}  
+			}  
+		}  
+		return flag;  
+	}  
 }
--- Backup/org/deta/db/resume/BootResume.java
+++ Backup/org/deta/db/resume/BootResume.java
@@ -2,6 +2,7 @@ package org.deta.db.resume;
 
 public class BootResume {
 	public static void resumeAllWithGzipByTime(long time){
-		
+		//get gzip file
+		//resume until to time;
 	}
 }
